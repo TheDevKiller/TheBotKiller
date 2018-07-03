4ff2871ef64934e4f7e78554e44e6841c49c6847
/TheDevKiller <thedevkiller@protonmail.com>
2018-07-04 00:10:44 +0200

    Ajout du changement de préfixe et des commandes "&code" et "&shifumi"

--- main.py
+++ main.py
@@ -29,5 +29,43 @@ async def on_ready:
 	thedevkiller = await client.get_user_infos("436105272310759426")
 	print("</TheBotKiller est prêt à discuter avec les utilisateurs et à jouer avec eux !")
 
+@client.event
+async def on_message(message): # Dès qu'il y a un message
+
+	if client.user.mentioned_in(message) and message.author == thedevkiller: # Changement du préfixe
+		global prefixe
+		prefixe = message.content.split(" ")
+		prefixe = str(prefixe[1])
+		await client.send_message(message.channel, "Mon préfixe est désormais" + " " + prefixe)
+		print("Le préfixe de </TheBotKiller> est désormais " + prefixe)
+
+	elif message.content.startswith(prefixe + "code"): # Renvoie le lien Github
+		await client.send_message(message.channel, "https://github.com/TheDevKiller/TheBotKiller")
+
+	elif message.content.startswith(prefixe + "shifumi"): # Commence une partie de Shifumi
+		elements = ["pierre", "feuille", "ciseaux"]
+		jeuJoueur = message.content.split(" ")[1].lower()
+		jeuBot = random.choice(elements)
+
+		if jeuJoueur == jeuBot:                                 # La deuxième personne
+			resultat = "Égalité"                            # du singulier signifie
+		elif jeuJoueur == "pierre" and jeuBot == "feuille":     # le joueur
+			resultat = "T'as perdu"
+		elif jeuJoueur == "pierre" and jeuBot == "ciseaux":
+			resultat = "T'as gagné"
+		elif jeuJoueur == "feuille" and jeuBot == "pierre":
+			resultat = "T'as gagné"
+		elif jeuJoueur == "ciseaux" and jeuBot == "pierre":
+			resultat = "T'as perdu"
+		elif jeuJoueur == "feuille" and jeuBot == "ciseaux":
+			resultat = "T'as perdu"
+		elif jeuJoueur == "ciseaux" and jeuBot == "Feuille":
+			resultat = "T'as gagné"
+		else:
+			await client.send_message(message.channel, "Entre un élément valide à la prochaine partie s'il te plaît :win$
+
+		await client.send_message(message.channel, embed=discord.Embed(title="Résultat du Shifumi entre " + message.author.n$
+
+
 
 client.run(token)
