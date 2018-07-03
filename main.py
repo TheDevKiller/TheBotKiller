6bd952cb5888e82aea67503a1c394a60d34740b1
/TheDevKiller <thedevkiller@protonmail.com>
2018-07-03 23:54:32 +0200

    Ajout de la connexion au serveur et du prefixe

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ with open("token.txt", "r") as tokenFile:
 	token = tokenFile.read()
 
 # /Token
+
+print("Chargement de </TheBotKiller> avec le token", token)
+
+client = discord.Client()
+
+global prefixe
+prefixe = "&"
+
+@client.event
+async def on_ready:
+	global thedevkiller
+	thedevkiller = await client.get_user_infos("436105272310759426")
+	print("</TheBotKiller est prêt à discuter avec les utilisateurs et à jouer avec eux !")
+
+
+client.run(token)
